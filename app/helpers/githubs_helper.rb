c1aabc11b3a0e9da5e758a550210078a40dd07ae
Peter Clark <peter@5clarks.net>
2014-09-13 14:42:38 -0500

    add chart helper

--- app/helpers/githubs_helper.rb
+++ app/helpers/githubs_helper.rb
@@ -1,2 +1,16 @@
 module GithubsHelper
+  
+  def commits_by_day_settings
+    { 
+      chart: { 
+        backgroundColor: "#FAFAFB" }, 
+      xAxis: { 
+        title: { 
+          text: "Last 30 Days" }},
+      yAxis: { 
+        title: { 
+          text: "Number of commits" }}
+    }
+  end
+  
 end
