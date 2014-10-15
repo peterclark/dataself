0fa7a5b3da15f0d132722d2d2970eda62df6e2b7
Peter Clark <peter@5clarks.net>
2014-10-14 20:39:54 -0500

    more fields from automatic

--- app/controllers/api/v1/text_messages_controller.rb
+++ app/controllers/api/v1/text_messages_controller.rb
@@ -5,11 +5,12 @@ class Api::V1::TextMessagesController < Api::V1::ApiController
     @text = text_message_params[:Text]
     
     case @text
-    when /^automatic (.+)/
-      @automatic = Automatic.create trip_map: $1
+    when /^automatic (.+),(.+),(.+),(.+),(.+)/
+      @automatic = Automatic.create trip_map: $1, avg_mpg: $2, miles: $3, duration: $4, fuel_volume: $5
       expose @automatic
     when /^football (.+),(.+),(.+),(.+)/
       @football = Football.create player: $1, team: $2, status: $3, player_url: $4
+      expose @football
     else
     end
   end
--- app/models/automatic.rb
+++ app/models/automatic.rb
@@ -3,8 +3,16 @@ class Automatic
   include Mongoid::Timestamps
   
   field :trip_map, type: String
+  field :avg_mpg, type: Float
+  field :miles, type: Float
+  field :duration, type: String
+  field :fuel_volume, type: Float
   
   validates :trip_map, presence: true
+  validates :avg_mpg, presence: true
+  validates :miles, presence: true
+  validates :duration, presence: true
+  validates :fuel_volume, presence: true
   
   scope :recent, ->(num) { desc(:created_at).limit(num) }
   
