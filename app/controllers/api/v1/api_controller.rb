62daf3fee8d6618b3cdf15260c35746f370ebb74
Peter Clark <peter@5clarks.net>
2015-08-17 20:10:27 -0500

    only auth on create

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ApiController < RocketPants::Base
   version 1
-  before_action :authenticate
+  before_action :authenticate, only: [:create]
 
   def authenticate
     error! :unauthenticated unless params[:secret] == Rails.application.secrets.api_secret
